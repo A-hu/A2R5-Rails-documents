126d29be48963c1007dc3b541fac23e115acf0dd
A-hu <louis0911317399@gmail.com>
2017-03-01 14:41:03 +0800

    How to Generate Data in a Rails 5 Application

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |d|
+  FreelanceDocument.create(title: "Document #{d}", 
+                           description: "I am an entrepreneur, engineer, CTO, and artisan with decades of startup experience, including co-founding Excite.com. He makes complex problems simple with expressive, maintainable code. He believes in building small, well-tested, functional pieces, loosely joined by a well-documented contract.",
+                           file_url: "https://docs.google.com/document/d/1UUgAob6ZpivgkgCbMD84JMMRc1flrzCPKybtQoAfASo/edit?usp=sharing",
+                           image_url: 'https://s3.amazonaws.com/devcamp-static/images/freelance-img.jpg'
+                          )
+  puts "FreelanceDocument #{d} created"
+end
